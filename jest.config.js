52891864eda46bacfb9a60c53ff8627a040fd798
Brian Ryall <brian@brianryall.xyz>
2021-07-26 17:07:31 -0400

    remove: old typescript stuff

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
-    collectCoverage: true,
-    coverageReporters: ["lcov"],
-    verbose: false,
-    setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.ts"]
-};
