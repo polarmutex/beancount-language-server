e755ce6de820da8ed101778d78b5457a9f58ad0e
Brian Ryall <brian@brianryall.xyz>
2022-04-28 13:13:47 -0400

    fix: fixes #53 only log with specified as an option

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,20 @@ mod server;
 use crate::core::logger::Logger;
 use std::path::PathBuf;
 
-fn cli() {
-    Command::new("beancount-language-server")
-        .arg(Arg::new("stdio").long("stdio"))
-        .get_matches();
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    cli();
+    let matches = Command::new("beancount-language-server")
+        .arg(Arg::new("stdio").long("stdio").help("use std io for lang server"))
+        //TODO let the user specify the file
+        .arg(Arg::new("log").long("log").help("Write logs to file"))
+        .get_matches();
 
-    let mut logger = Logger::new().unwrap();
-    logger
-        .set_path(Some(PathBuf::from("beancount-langserver.log")))
-        .expect("Could not open log file");
+    if matches.is_present("log") {
+        let mut logger = Logger::new().unwrap();
+        logger
+            .set_path(Some(PathBuf::from("beancount-langserver.log")))
+            .expect("Could not open log file");
+    }
 
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
