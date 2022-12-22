6f1cd97e39880a4987d599aeb1c27e64ce66ac03
Brian Ryall <brian@brianryall.xyz>
2022-12-21 21:34:51 -0500

    refactor: simplify logging

--- crates/lsp/src/main.rs
+++ crates/lsp/src/main.rs
@@ -1,12 +1,15 @@
 use beancount_language_server::server::run_server;
 use clap::{Arg, Command};
-use tracing::{debug_span, level_filters::LevelFilter, Instrument};
-use tracing_subscriber::{filter::Directive, layer::SubscriberExt, EnvFilter};
-use tracing_tree::HierarchicalLayer;
+use std::fs;
+use std::io;
+use std::sync::Arc;
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::fmt::writer::BoxMakeWriter;
+use tracing_subscriber::{filter::Directive, EnvFilter};
 
 #[tokio::main]
 async fn main() {
-    let _matches = Command::new("beancount-language-server")
+    let matches = Command::new("beancount-language-server")
         .arg(
             Arg::new("stdio")
                 .long("stdio")
@@ -16,49 +19,33 @@ async fn main() {
         .arg(Arg::new("log").long("log").help("Write logs to file"))
         .get_matches();
 
-    let filter = EnvFilter::default().add_directive(Directive::from(LevelFilter::DEBUG));
-
-    let (file_logger, _guard, std_logger) = if _matches.contains_id("log") {
-        println!("enable logging");
-        let file_appender = tracing_appender::rolling::never(".", "beancount-language-server.log");
-        let (non_blocking, guard) = tracing_appender::non_blocking(file_appender);
-        (
-            Some(
-                HierarchicalLayer::default()
-                    .with_indent_lines(true)
-                    .with_indent_amount(4)
-                    .with_bracketed_fields(true)
-                    .with_ansi(false)
-                    .with_writer(non_blocking),
-            ),
-            Some(guard),
-            None,
-        )
-    } else {
-        (
-            None,
-            None,
-            Some(
-                HierarchicalLayer::default()
-                    .with_indent_lines(true)
-                    .with_indent_amount(4)
-                    .with_bracketed_fields(true),
-            ),
-        )
-    };
-
-    let subscriber = tracing_subscriber::registry()
-        .with(filter)
-        .with(file_logger)
-        .with(std_logger);
-    tracing::subscriber::set_global_default(subscriber).unwrap();
-
-    let span = debug_span!("Running Beancount LSP Server", pid = std::process::id());
-    tracing::debug!("Test");
-    tracing::info!("Test");
+    setup_logging(matches.contains_id("log"));
 
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    run_server(stdin, stdout).instrument(span).await
+    run_server(stdin, stdout).await
+}
+
+fn setup_logging(file: bool) {
+    let file = if file {
+        fs::OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("beancount-language-server.log")
+            .ok()
+    } else {
+        None
+    };
+
+    let writer = match file {
+        Some(file) => BoxMakeWriter::new(Arc::new(file)),
+        None => BoxMakeWriter::new(io::stderr),
+    };
+
+    let filter = EnvFilter::default().add_directive(Directive::from(LevelFilter::DEBUG));
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_writer(writer)
+        .init();
 }
