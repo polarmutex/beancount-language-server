02cfd8274461b868257505b0228fe0f889957b20
Brian Ryall <brian@brianryall.xyz>
2023-10-09 22:37:17 -0400

    test: add tests for date completion logic

--- crates/lsp/src/providers/completion.rs
+++ crates/lsp/src/providers/completion.rs
@@ -213,3 +213,37 @@ fn complete_account(
     }
     Ok(Some(lsp_types::CompletionResponse::Array(completions)))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::add_one_month;
+    use super::sub_one_month;
+
+    #[test]
+    fn handle_sub_one_month() {
+        let input_date = chrono::NaiveDate::from_ymd_opt(2022, 6, 1).expect("valid date");
+        let expected_date = chrono::NaiveDate::from_ymd_opt(2022, 5, 1).expect("valid date");
+        assert_eq!(sub_one_month(input_date), expected_date)
+    }
+
+    #[test]
+    fn handle_sub_one_month_in_jan() {
+        let input_date = chrono::NaiveDate::from_ymd_opt(2022, 1, 1).expect("valid date");
+        let expected_date = chrono::NaiveDate::from_ymd_opt(2021, 12, 1).expect("valid date");
+        assert_eq!(sub_one_month(input_date), expected_date)
+    }
+
+    #[test]
+    fn handle_add_one_month() {
+        let input_date = chrono::NaiveDate::from_ymd_opt(2022, 6, 1).expect("valid date");
+        let expected_date = chrono::NaiveDate::from_ymd_opt(2022, 7, 1).expect("valid date");
+        assert_eq!(add_one_month(input_date), expected_date)
+    }
+
+    #[test]
+    fn handle_add_one_month_in_dec() {
+        let input_date = chrono::NaiveDate::from_ymd_opt(2021, 12, 1).expect("valid date");
+        let expected_date = chrono::NaiveDate::from_ymd_opt(2022, 1, 1).expect("valid date");
+        assert_eq!(add_one_month(input_date), expected_date)
+    }
+}
